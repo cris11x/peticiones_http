99a7b7cedba0d7a46a6e922a23a1170788c82a5f
Cristian <Cristian>
2021-02-26 11:40:44 -0500

    Subir Fotografia

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
-import { init } from './js/usuarios-page';
+//import * as CRUD from './js/crud-provider';
+import { init  } from './js/archivos-page';
+
+/*CRUD.getUsusario( 1 ).then( console.log );*/
 
 init();
 
+/*CRUD.crearUsuario({
+    name: 'Cristian',
+    job: 'Programador'
+}).then( console.log );
 
 
+CRUD.actualizarUsuario( 1, { 
+    name: 'Sara',
+    job: 'Developer'
+}).then( console.log );*/
+
+//CRUD.borrarUsuario( 1 ).then( console.log );
--- src/js/http-priovider.js
+++ src/js/http-priovider.js
@@ -2,6 +2,10 @@
 const jokeUrl       = 'https://api.chucknorris.io/jokes/random';
 const urlUsuarios   = 'https://reqres.in/api/users?page=2';
 
+//Cloudinary
+const cloudPresent  = 'ebjmb2v3';
+const cloudUrl      = 'https://api.cloudinary.com/v1_1/cris10x/upload';
+
 
 const obtenerChiste = async() => {
     
@@ -33,7 +37,33 @@ const obtenerUsuarios = async() => {
 
 }
 
+const subirImagen = async( archivoSubir ) => {
+
+    const formData = new FormData();
+    formData.append('upload_preset', cloudPresent );
+    formData.append('file', archivoSubir );
+
+    try {
+
+        const resp = await fetch( cloudUrl, {
+            method: 'POST',
+            body: formData
+        });
+
+        if ( resp.ok ) {
+            const cloudResp = await resp.json();
+            return cloudResp.secure_url;
+        } else {
+            throw await resp.json();
+        }
+
+    } catch( err ) {
+        throw err;
+    }
+}
+
 export {
     obtenerChiste,
-    obtenerUsuarios
+    obtenerUsuarios,
+    subirImagen
 }
